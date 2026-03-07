6ce50fedc39345e9a69a7639335c7f207821292a
Devika <devika2005uk@gmail.com>
2026-03-08 00:33:59 +0530

    update auth pages, onboarding help focus, and API client

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import { getAuthToken } from "../auth/token";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "http://localhost:5000";
 
 const apiClient = async (endpoint, { body, ...customConfig } = {}) => {
     const token = getAuthToken();
