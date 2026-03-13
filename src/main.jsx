27dce039a233ae1d8cfad2c7475f45906b92bddb
Devika <devika2005uk@gmail.com>
2026-03-13 11:04:29 +0530

    feat: implement google login/signup on frontend

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { OnboardingProvider } from "./pages/onBoarding/OnboardingContext.jsx";  
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-         <OnboardingProvider>
-        <App />
-      </OnboardingProvider>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <OnboardingProvider>
+          <App />
+        </OnboardingProvider>
+      </GoogleOAuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
